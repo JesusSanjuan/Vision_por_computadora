6ab5baf6877dac44e6e8584e6e1f5fa3e5fc5663
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-03-28 20:02:22 -0600

    Tarea de fuirier

--- Vision_por_computadora/FiltroFurier.cpp
+++ Vision_por_computadora/FiltroFurier.cpp
@@ -4,37 +4,139 @@
 #include "opencv2/highgui/highgui_c.h"
 #include <stdlib.h>
 #include <math.h>
+#define M_PI 3.14159265358979323846
 
 using namespace cv;
 
 int main(int, char**)
 {
 
-	namedWindow("Grises", 1);
-	namedWindow("Mascara", 1);
+	
 
 
-	Mat color = imread("image2.jpg");// CV_LOAD_GRAYSCALE
-	Mat gris = Mat(color.rows, color.cols, CV_8U);
-	Mat furier = Mat(color.rows, color.cols, CV_8U);
-	cvtColor(color, gris, CV_RGB2GRAY);
-	for (;;)
-	{		
+	Mat modificada = cv::imread("image0.jpg");
+	cv::cvtColor(modificada, modificada, CV_BGR2GRAY);
+	Mat original = modificada.clone();
 
-		for (int x = 0; x < gris.cols - 1; x++) // Control de ventana
+	int M = original.cols;//width
+	int N = original.rows;//high
+	printf("imagen tamaño width: %d  y high: %d \n", M, N);
+	//paso 1:f(x,y)*(-1)^(x+y)
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
 		{
-			for (int y = 0; y < gris.rows - 1; y++) // Control de ventana
-			{
-			
-				double intensidad = gris.at<uchar>(y, x);
-				furier.at<uchar>(y, x) = intensidad * pow((-1),(y+x));                                                                                                                                                                                                                                                                                                      
-						
-			}
+			modificada.at<uchar>(y, x) = original.at<uchar>(y, x)*pow(-1.0, (double)x + y);
 		}
-		imshow("Grises", gris);
-		imshow("Mascara", furier);
-		waitKey(30);
 	}
+	
+	Mat modificada2;
+	modificada.convertTo(modificada2, CV_64F);
+
+	Mat DFT = modificada2.clone();
+	Mat DFTImageRE = modificada2.clone();
+	Mat DFTImageIM = modificada2.clone();
+	Mat DFTInverse = Mat(modificada2.rows, modificada2.cols, CV_64F, Scalar(0));
+	Mat salida = Mat(modificada2.rows, modificada2.cols, CV_8U);
+
+	//Paso 2: Aplicamos la transformada
+
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			double ak = 0.0;
+			double bk = 0.0;
+			for (int j = 0; j < M; j++)
+			{
+				for (int k = 0; k < N; k++)
+				{
+
+					double divM = double(x*j) / double(M);
+					double divN = double(y*k) / double(N);
+
+					double equiz = 2.0 * M_PI * divM;
+					double ye = 2.0 * M_PI * divN;
+
+					ak += modificada2.at<double>(k, j)*cos(equiz + ye);
+					bk += modificada2.at<double>(k, j)*(-1.0)*sin(equiz + ye);
+
+				}
+			}
+
+			double ResX = 0.0, ResY = 0.0;
+			double div = double(1.0) / double(M*N);
+			ResX = double(ak)*div;
+			ResY = double(bk)*div;//(1/M*N)
+
+			DFTImageRE.at<double>(y, x) = ResX;
+			DFTImageIM.at<double>(y, x) = ResY;
+
+			double cuadrados = pow(ResX, 2.0) + pow(ResY, 2.0);
+			DFT.at<double>(y, x) = sqrt(cuadrados);
+			//DFT.at<double>(x, y) = abs(ResX) + abs(ResY);
+
+		}
+	}
+
+	printf("Termino Conversion de Tranformada de Furier");
+	//Paso 3: Aplicamos la inversa
+	for (int x = 0; x < M; x++)
+	{
+		for (int y = 0; y < N; y++)
+		{
+			double akR = 0;
+			double bkR = 0;
+
+			double akI = 0;
+			double bkI = 0;
+			for (int j = 0; j < M; j++)
+			{
+				for (int k = 0; k < N; k++)
+				{
+
+					double divM = double(x*j) / double(M);
+					double divN = double(y*k) / double(N);
+
+					double equiz = 2.0*M_PI * divM;
+					double ye = 2.0*M_PI * divN;
+
+
+					akR += DFTImageRE.at<double>(k, j)*cos(equiz + ye);
+					bkR += DFTImageRE.at<double>(k, j)*(1.0)*sin(equiz + ye);
+
+					akI += DFTImageIM.at<double>(k, j)*cos(equiz + ye);
+					bkI += DFTImageIM.at<double>(k, j)*(1.0)*sin(equiz + ye);
+				}
+			}
+			
+			double sumReal = akR + bkI;
+			double sumImag = bkR + akI;
+
+			DFTInverse.at<double>(y, x) = abs(sumReal) + abs(sumImag);
+		}
+	}
+
+	DFTInverse.convertTo(salida, CV_8U);
+
+	for (int x = 0; x < DFTInverse.cols; x++)
+	{
+		for (int y = 0; y < DFTInverse.rows; y++)
+		{
+			salida.at<uchar>(y, x) = (salida.at<uchar>(y, x)*pow(-1.0, (double)x + y));
+		}
+	}
+
+	imshow("Original", original);
+	imshow("Modificada", modificada);
+	imshow("Modificada2", modificada2);
+	imshow("DFT", DFT);
+	//imshow("DFTImageRE", DFTImageRE);
+	//imshow("DFTImageIM", DFTImageIM);
+	imshow("DFTinvertida", DFTInverse);
+	imshow("OriginalProce", salida);
+	waitKey(0);
+	
 	return 0;
 
 }
