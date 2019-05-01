8cd5a838ce7c1f30cc1fb988619964f8c7b087b4
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-05-01 13:20:14 -0500

    Dilatacion completa

--- Vision_por_computadora/Dilatacion.cpp
+++ Vision_por_computadora/Dilatacion.cpp
@@ -5,22 +5,20 @@
 using namespace cv;
 
 const int alpha_slider_max = 255;
-int alpha_slider=2;
+int alpha_slider=100;
 
-int Mascara[3][3] = { { 0,  1,  0},
-					  { 1,  1,  1}, /* los espacios y saltos de líneas no son tomados en cuenta */
-					  { 0,  1,  0} };
+int Mascara[3][3] = { { 1,  0,  1},
+					  { 0,  0,  0}, /* los espacios y saltos de líneas no son tomados en cuenta */
+					  { 1,  0,  1} };
 
 void on_trackbar(int, void*)
 {
 	std::cout << "Barra 1: " << alpha_slider << std::endl;
 
-
-	Mat grayscale = cv::imread("Captura.jpg", IMREAD_GRAYSCALE);
-
+	Mat grayscale = cv::imread("image2.jpg", IMREAD_GRAYSCALE);
 	Mat binaria = Mat(grayscale.rows, grayscale.cols, CV_8U);
-	Mat Dilatacion = Mat(grayscale.rows, grayscale.cols, CV_8U);
-
+	Mat Dilatacion = Mat(grayscale.rows, grayscale.cols, CV_64F);
+	
 	int rango1 = alpha_slider;
 
 	for (int x = 0; x < grayscale.cols; x++)
@@ -31,42 +29,41 @@ void on_trackbar(int, void*)
 			
 			if (intensidad <= rango1)
 			{
-				//std::cout << "Intensidad: " << intensidad << " <= Rango " << rango1 << std::endl;
 				binaria.at<uchar>(y, x) = 1;
 			}
 			else
-			{
-				
+			{				
 				binaria.at<uchar>(y, x) = 0;
 			}
 		}
 	}
 	binaria.convertTo(binaria, CV_64F);
-	/*double p1 = 0; int resu = 0; int tem1, tem2; int ancho = grayscale.cols; int alto = grayscale.rows; 
-	for (int i = 1; i < ancho - 1; i++)
+
+	double p1 = 0; int resu = 0; int tem1, tem2; 
+	for (int i = 1; i < grayscale.cols - 1; i++)
 	{
-		for (int j = 1; j < alto - 1; j++)
+		for (int j = 1; j < grayscale.rows - 1; j++)
 		{
 			p1 = binaria.at<double>(j, i);
-			if (0 == p1)
+			if (1 == p1)
 			{
-				tem1 = j; tem2 = i;
+				tem1 = i; tem2 = j;
 				for (int ii = 0; ii < 3; ii++)
 				{
 					for (int jj = 0; jj < 3; jj++)
 					{
-						resu = Mascara[ii][jj];
+						resu = Mascara[jj][ii];
 						if (resu == 1)
 						{
-							Dilatacion.at<uchar>(tem1 - 1, tem2 - 1) = resu;
-						}tem2++;
-					}tem1++;    tem2 = i;
+							int x = tem1 - 1; int y = tem2 - 1;
+							Dilatacion.at<double>(y, x) = resu;
+						}
+					    tem2++;
+					}tem1++;    tem2 = j;
 				}
 			}
 		}
 	}
-	Dilatacion.convertTo(Dilatacion, CV_64F);*/
-
 	imshow("grayscale", grayscale);
 	imshow("Binaria", binaria);
 	imshow("Dilatacion", Dilatacion);
@@ -81,8 +78,6 @@ int main(int, char**)
 	createTrackbar("Rango 1", "grayscale", &alpha_slider, alpha_slider_max, on_trackbar );
 		
 	waitKey(0);
-
     return 0;
-
 }
 
